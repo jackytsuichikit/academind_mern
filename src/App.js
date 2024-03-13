cb66dc4fe8ae3a545f388ef8411453e6a7c639ba
Jacky <jacky.ck.tsui@gmail.com>
2024-03-13 03:27:56 +0000

    message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import GoalList from './components/GoalList/GoalList';
+import './App.css';
+
 const App = () => {
-  return <h1>A react App!</h1>;
+  const courseGoals = 
+  [{id: 'cg1', text: 'Finish the course'},
+   {id: 'cg2', text: 'Learn all about the course main topic'}, 
+   {id: 'cg3', text: 'Help other students in the course Q&A'}
+  ];
+
+  return (
+  <div className="course-goals">
+    <h2>Course Goals</h2>
+    <GoalList goals={courseGoals} />
+  </div>
+  );
+  
+
+  
 };
 
 export default App;
